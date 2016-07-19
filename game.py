453f9a307ec6189617a03298f4b4bb3c27042e8b
Szymon Kadłuczka <szymon.kadluczka@gmail.com>
2016-07-19 11:41:17 +0200

    Player movement

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ class Game(object):
 		self.width = 640
 		self.height = 480
 		self.gamestate = 1
+		self.keypressed = 0
 		self.level = "level1"
 
 		MAP = level_data["level1"]
@@ -44,14 +45,45 @@ class Game(object):
 				tile = self.current_map[x][y].block_type
 				self.surface.blit(tiles[tile], (x*64,y*64))
 
+	def move(self, x, y, angle):
+		if self.player.x + x >= 0 and self.player.x + x < MAP_WIDTH and self.player.y + y >= 0 and self.player.y + y < MAP_HEIGHT:
+
+			next_step = self.current_map[self.player.x + x][self.player.y + y].block_type
+			next_pos =  (self.current_map[self.player.x + x][self.player.y + y].x, self.current_map[self.player.x + x][self.player.y + y].y)
+
+		 	if next_step == 1:
+				self.swap(x, y)
+
+	def swap(self, x, y):
+		self.current_map[self.player.x + x][self.player.y + y].block_type = 0
+		self.current_map[self.player.x][self.player.y].block_type = 1
+		self.player.x += x
+		self.player.y += y
+
 	def loop(self):
 		while self.gamestate == 1:
 			for event in pygame.event.get():
 				if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
 					self.gamestate = 0
 
+				if event.type == KEYDOWN and event.key == pygame.K_s and self.keypressed == 0:
+					self.move(0, 1, 0)
+					self.keypressed = 1
+				if event.type == KEYDOWN and event.key == pygame.K_w and self.keypressed == 0:
+					self.move(0, -1, 180)
+					self.keypressed = 1
+				if event.type == KEYDOWN and event.key == pygame.K_a and self.keypressed == 0:
+					self.move(-1,0, 270)
+					self.keypressed = 1
+				if event.type == KEYDOWN and event.key == pygame.K_d and self.keypressed == 0:
+					self.move(1,0, 90)
+					self.keypressed = 1
+
 				self.drawMap()
 				pygame.display.flip()
+
+				if event.type == KEYUP:
+					self.keypressed = 0
 
 if __name__ == '__main__':
 	Game()
