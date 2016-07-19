563fecfe33e60de0cab5e3e614b6e8e7da4e6883
Szymon Kadłuczka <szymon.kadluczka@gmail.com>
2016-07-19 11:58:40 +0200

    Added water (barrier)

--- levels.py
+++ levels.py
@@ -1,6 +1,6 @@
 import pygame
 
-level1 = [[1,1,1,1,1,1,1,1,1,1],
+level1 = [[2,2,2,2,1,1,1,1,1,1],
       	  [1,1,1,1,1,1,1,1,1,1],
 	      [1,1,1,1,1,1,1,1,1,1],
 	      [1,1,1,1,1,1,1,1,1,1],
@@ -18,3 +18,4 @@ level1_y = 1
 tiles = [None] * 25
 tiles[0] = pygame.image.load('images/player.png')
 tiles[1] = pygame.image.load('images/tile1.png')
+tiles[2] = pygame.image.load('images/water.png')
